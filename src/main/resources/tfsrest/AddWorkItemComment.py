62ba1d664c690fffc1cbe531d464b41bff7a7679
Ravan Naidoo <rnaidoo@xebialabs.com>
2016-12-08 13:40:57 +0100

    Added ability to add comment to work item.

--- src/main/resources/tfsrest/AddWorkItemComment.py
+++ src/main/resources/tfsrest/AddWorkItemComment.py
@@ -5,7 +5,6 @@
 #
 
 import sys
-import com.xhaus.jyson.JysonCodec as json
 
 print "Executing CreateWorkItem.py ver 2015Jun28-1"
 
@@ -16,16 +15,14 @@ if tfsServer is None:
 contentType = 'application/json-patch+json'
 
 request = HttpRequest(tfsServer)
-title_json = '{"path": "/fields/System.Title", "value": "%s", "op": "add"}' % workItemTitle
-description_json = '{"path": "/fields/System.Description", "value": "%s", "op": "add"}' % workItemDescription
-content = '[%s, %s]' % (title_json, description_json)
+comment_json = '{"path": "/fields/System.History", "value": "%s", "op": "add"}' % workItemComment
+content = '[%s]' % (comment_json)
 # define TFS Server url as http://server:port/tfs
-response = request.patch('%s/%s/_apis/wit/workitems/$%s?api-version=1.0' % (collectionName, teamProjectName, workItemType), content, contentType=contentType)
+response = request.patch('%s/_apis/wit/workitems/%s?api-version=1.0' % (collectionName, workItemId), content, contentType=contentType)
 httpStatusCode = response.status
 
 if httpStatusCode == 200:
-  workItemId = json.loads(response.response)['id']
-  print "New work item id is %d" % workItemId
+  print "Comment updated."
 else:
   print "Error in creating Work Item. Return status %s" % httpStatusCode
   print response.response
